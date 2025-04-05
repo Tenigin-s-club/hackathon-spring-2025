a59918b6bcebb39a000c74c2ffeeef43f582b46b
lifelongCat <lifelongcat@yandex.ru>
2025-04-06 01:44:28 +0300

    add default value for question solution

--- backend/src/database/models/question.py
+++ backend/src/database/models/question.py
@@ -11,7 +11,7 @@ class Question(Base):
 
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, server_default=text('uuid_generate_v4()'))
     title: Mapped[str]
-    description: Mapped[str]
+    description: Mapped[str] = mapped_column(default='')
     solution: Mapped[str]
     meeting_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('meeting.id'))
     meeting: Mapped["Meeting"] = relationship(back_populates="questions")
