ead7d605b6599e9122d688a7239bb27fcb98f7c3
lifelongCat <lifelongcat@yandex.ru>
2025-04-05 10:27:10 +0300

    create migration for meeting models

--- backend/src/database/models/question.py
+++ backend/src/database/models/question.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 
 from sqlalchemy import text, ForeignKey
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from src.database.config import Base
 
@@ -11,9 +11,7 @@ class Question(Base):
 
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, server_default=text('uuid_generate_v4()'))
     title: Mapped[str]
+    description: Mapped[str]
     solution: Mapped[str]
-    place: Mapped[str]
-    is_internal: Mapped[bool]
-    protocol_datetime: Mapped[datetime | None]
-    counter: Mapped[str]
     meeting_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('meeting.id'))
+    meeting: Mapped["Meeting"] = relationship(back_populates="questions")
