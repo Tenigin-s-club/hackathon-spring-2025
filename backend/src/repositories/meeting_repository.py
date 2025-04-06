d071f065c08f454d0349c8694a939fcf69ec89fb
k <iliakripa@mail.ru>
2025-04-06 14:21:58 +0300

    fix statistic 4

--- backend/src/repositories/meeting_repository.py
+++ backend/src/repositories/meeting_repository.py
@@ -53,4 +53,4 @@ class MeetingRepository:
             query = select(Question).where(Question.meeting_id == id)
             questions = await session.execute(query)
 
-            return questions.unique().mappings().all()
+            return questions.unique().all()
