f636b8690799824065724006f5f305bd15324fd7
lizakobzeva <lizakobzeva@list.ru>
2025-04-06 03:45:58 +0300

    add more data for MeetingPage

--- frontend/src/pages/MeetingPage/MeetingPage.tsx
+++ frontend/src/pages/MeetingPage/MeetingPage.tsx
@@ -10,32 +10,49 @@ const MeetingPage = () => {
   if (!data) {
     return <div>Не получилось получить данные заседения</div>;
   }
+  const nowDate = new Date();
+  let status = "active";
+  if (
+    nowDate < new Date(data.end_datetime) &&
+    nowDate > new Date(data.voting_datetime)
+  ) {
+    status = "active";
+  } else if (nowDate > new Date(data.end_datetime)) {
+    status = "completed";
+  } else {
+    status = "future";
+  }
   return (
-    <div>
-      <h2 className="text-3xl font-bold text-center my-4">Заседание №{id}</h2>
-      <p>
-        <span className="font-bold">Место:</span> {data.place}
-      </p>
-      <p>
-        <span className="font-bold">Формат:</span>
-        {data.is_internal ? "очно" : "очно-заочное"}
-      </p>
-      <p>
-        <span className="font-bold">Дата начала:</span>
-        {format(data.voting_datetime, fullDateRuFormat)}
-      </p>
-      {status === "completed" && (
+    <div className="flex items-center justify-center w-full">
+      <div className="flex flex-col gap-4 w-[70%] my-auto min-w-[300px]">
+        <h2 className="text-3xl font-bold text-center my-4">Заседание №{id}</h2>
+
+        {status === "active" && <Button>ПОДКЛЮЧИТЬ ВКС</Button>}
+        <p>
+          <span className="font-bold">Место:</span> {data.place}
+        </p>
+        <p>
+          <span className="font-bold">Формат:</span>
+          {data.is_internal ? "очно" : "очно-заочное"}
+        </p>
+        <p>
+          <span className="font-bold">Дата начала:</span>
+          {format(data.voting_datetime, fullDateRuFormat)}
+        </p>
         <p>
           <span className="font-bold">Дата окончания:</span>
           {format(data.end_datetime, fullDateRuFormat)}
         </p>
-      )}
-      <div className="mt-2 flex w-full justify-end">
-        {status === "active" && <Button>Войти</Button>}
-        {status === "future" && <Button variant="ghost">Скоро</Button>}
-        {status === "completed" && (
-          <Button variant="secondary">Просмотр</Button>
-        )}
+        <p>
+          <span className="font-bold">Всего вопросов:</span>
+          {data.questions.length}
+        </p>
+        {data.questions.map((question) => (
+          <div className="border-grey-300 border-[1px] rounded-xl p-4 flex justify-between">
+            <p>{question.title}</p>
+            <Button>Ответить</Button>
+          </div>
+        ))}
       </div>
     </div>
   );
